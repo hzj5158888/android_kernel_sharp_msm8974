75260f1fbeeb103f018935d34ec9439d2e7ab5c0
JerryTsang <jerryzheng310@gmail.com>
2017-03-23 16:13:48 +0800

    dma-contiguous: Fix mismerge
    Change-Id: Iaf4ace2e779fffabf5db2e798e1535406a775153

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -356,16 +356,8 @@ void __init dma_contiguous_reserve(phys_addr_t limit)
 				i--;
 				continue;
 			}
-<<<<<<< HEAD
-<<<<<<< HEAD
 			dma_contiguous_early_fixup(cma_areas[i].base,
 							cma_areas[i].size);
-=======
->>>>>>> 611a97e... cma: Delay non-placed memblocks until after all allocations
-=======
-			dma_contiguous_early_fixup(cma_areas[i].base,
-							cma_areas[i].size);
->>>>>>> 98a5e54... cma: Call dma_contiguous_early_fixup after allocation
 		}
 
 		pr_info("CMA: reserved %ld MiB at %pa for %s\n",
